e9d91dfb9313b88e9e8c3aaa369fb7dcc751a327
Guga Villar <grsv.21@gmail.com>
2015-05-05 17:09:30 -0300

    Arquivo com uso da função criaBox

--- js/delProduto.js
+++ js/delProduto.js
@@ -3,7 +3,14 @@
 /*global formSubmit*/
 /*global confirma*/
 formSubmit("excluir", "POST", "./sys/delProduto.php");
-criaSelect("produtos", "./sys/include/select.php", "id_produto", "nome_produto");
+criaSelect("categorias", "./sys/include/select.php", "id_categoria", "nome_categoria");
+document.getElementById("categorias").addEventListener("change", function () {
+    'use strict';
+    while (document.getElementById("produtos").hasChildNodes()) {
+        document.getElementById("produtos").removeChild(document.getElementById("produtos").firstChild);
+    }
+    criaBox("./sys/include/select.php", "id_produto", "nome_produto", "produtos", document.getElementById("categorias").value);
+});
 function trataDados(resposta) {
     'use strict';
     if (resposta.excluir.valor) {
