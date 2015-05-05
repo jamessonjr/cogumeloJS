faf9ca5755523f97f59a862564ebb2e793d21729
Guga Villar <grsv.21@gmail.com>
2015-05-05 17:10:24 -0300

    Mudança para deletar vários registros no banco

--- sys/delProduto.php
+++ sys/delProduto.php
@@ -1,7 +1,9 @@
 <?
 include("include/conecta.php");
 if (isset($_POST)) {
-    $retorno = deleteBanco("produtos", "id_produto", $_POST["id_produto"]);
+    foreach ($_POST as $chave => $valor){
+        $retorno = deleteBanco("produtos", "id_produto", $_POST[$chave]);
+    }
     $var = array("excluir" => array("valor" => $retorno));
     echo json_encode($var);
     mysqli_close($link);
