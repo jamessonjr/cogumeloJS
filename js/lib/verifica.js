f84d4d89ba208f6a31befb8d77a2c8c63ae5d4ef
Guga Villar <grsv.21@gmail.com>
2015-05-06 21:07:18 -0300

    Colocação de um if na função

--- js/lib/verifica.js
+++ js/lib/verifica.js
@@ -14,9 +14,7 @@ var n = {
 //FUNÇÃO PARA VERIFICAR O LOGIN DO USUÁRIO, O SEU NÍVEL DE ACESSO
 (function () {
     'use strict';
-    var dados = "usuario=" + window.sessionStorage.getItem("x") + "&" + "senha=" + window.sessionStorage.getItem("y"),
-        resposta,
-        xmlhttp = new XMLHttpRequest();
+    var resposta, xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             resposta = JSON.parse(xmlhttp.responseText);
@@ -35,30 +33,32 @@ var n = {
     };
     xmlhttp.open("POST", "./sys/login.php", true);
     xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xmlhttp.send(dados);
+    xmlhttp.send("usuario=" + window.sessionStorage.getItem("x") + "&" + "senha=" + window.sessionStorage.getItem("y"));
 }());
 
 //FUNÇÃO PARA VERIFICAR SE DADO INSERIDO NO CAMPO JÁ EXISTE NO BANCO
 function verifica(idCampo, url, msgErro) {
     'use strict';
     var nomeArray, dadoArray, resposta, xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            resposta = JSON.parse(xmlhttp.responseText);
-            //window.alert(xmlhttp.responseText); //DESCOMENTE PARA GERAR UM ALERTA DOS DADOS
-            for (nomeArray in resposta) {
-                if (resposta.hasOwnProperty(nomeArray)) {
-                    for (dadoArray in resposta[nomeArray]) {
-                        if (resposta[nomeArray].hasOwnProperty(dadoArray)) {
-                            if (document.getElementById(idCampo).value === resposta[nomeArray][dadoArray].toString()) {
-                                confirma(0, "", "", "", msgErro);
+    if (document.getElementById(idCampo).value) {
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                resposta = JSON.parse(xmlhttp.responseText);
+                //window.alert(xmlhttp.responseText); //DESCOMENTE PARA GERAR UM ALERTA DOS DADOS
+                for (nomeArray in resposta) {
+                    if (resposta.hasOwnProperty(nomeArray)) {
+                        for (dadoArray in resposta[nomeArray]) {
+                            if (resposta[nomeArray].hasOwnProperty(dadoArray)) {
+                                if (document.getElementById(idCampo).value === resposta[nomeArray][dadoArray].toString()) {
+                                    confirma(0, "", "", "", msgErro);
+                                }
                             }
                         }
                     }
                 }
             }
-        }
-    };
+        };
+    }
     xmlhttp.open("POST", url, true);
     xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xmlhttp.send(document.getElementById(idCampo).getAttribute('name') + "=" + document.getElementById(idCampo).value);
